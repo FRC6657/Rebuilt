328609f0307d26549837797ec819bc175392defe
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2026-01-17 10:54:03 -0800

    add Encoder

--- src/main/java/frc/robot/subsystems/intake/IntakeIO_Real.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO_Real.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.reduxrobotics.sensors.canandmag.Canandmag;
 
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
@@ -12,10 +13,15 @@ import edu.wpi.first.math.util.Units;
 public class IntakeIO_Real {
     private TalonFX extMotor;
 
+    private Canandmag encoder;
+
     private double angleSetpoint = IntakeConstants.ExtensionMotor.maxLength;
     private double speedSetpoint = 0;
 
     public IntakeIO_Real() {
+        extMotor = new TalonFX(IntakeConstants.CAN.ExtensionMotor.id);
+        encoder = new Canandmag(IntakeConstants.CAN.Encoder.id);
+        
         var extConfigurator = extMotor.getConfigurator();
         var extConfigs = new TalonFXConfiguration();
         extConfigs.CurrentLimits = IntakeConstants.ExtensionMotor.extCurrentConfigs;
