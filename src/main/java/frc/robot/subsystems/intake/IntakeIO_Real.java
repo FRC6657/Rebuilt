3ca1e95ed23da27d2ce1bbd83b7137272267afd5
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2026-01-17 09:42:26 -0800

    define motor controllers

--- src/main/java/frc/robot/subsystems/intake/IntakeIO_Real.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO_Real.java
@@ -1,5 +1,20 @@
 package frc.robot.subsystems.intake;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.math.util.Units;
+
 public class IntakeIO_Real {
+    private TalonFX extMotor;
+    
+    private double angleSetpoint = IntakeConstants.maxLength;
+    private double speedSetpoint = 0;
+
+    private ProfiledPIDController extPID =
+        new ProfiledPIDController(
+            13.5, 0, 0, new Constraints(Units.inchesToMeters(12), Units.inchesToMeters(10))
+
+        );
     
 }
