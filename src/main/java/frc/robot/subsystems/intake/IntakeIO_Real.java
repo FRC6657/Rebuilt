176688e715a2d77948f0dd2a64cbfcb7d93dc4fd
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2026-01-17 09:58:58 -0800

    fix issues with real, add current limits

--- src/main/java/frc/robot/subsystems/intake/IntakeIO_Real.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO_Real.java
@@ -1,17 +1,25 @@
 package frc.robot.subsystems.intake;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.math.util.Units;
 
 public class IntakeIO_Real {
-  private TalonFX extMotor;
+    private TalonFX extMotor;
 
-  private double angleSetpoint = IntakeConstants.maxLength;
-  private double speedSetpoint = 0;
+    private double angleSetpoint = IntakeConstants.ExtensionMotor.maxLength;
+    private double speedSetpoint = 0;
 
-  private ProfiledPIDController extPID =
-      new ProfiledPIDController(
-          13.5, 0, 0, new Constraints(Units.inchesToMeters(12), Units.inchesToMeters(10)));
+    private ProfiledPIDController extPID =
+        new ProfiledPIDController(
+            13.5, 0, 0, new Constraints(Units.inchesToMeters(12), Units.inchesToMeters(10))
+    );
+
+    public IntakeIO_Real() {
+        var extConfigurator = extMotor.getConfigurator();
+        var extConfigs = new TalonFXConfiguration();
+        extConfigs.CurrentLimits = IntakeConstants.ExtensionMotor.extCurrentConfigs;
+    }
 }
