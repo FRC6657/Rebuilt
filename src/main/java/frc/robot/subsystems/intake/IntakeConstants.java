406f9bfb2a67c9416fea802c2477b28fc56b7618
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2026-01-17 09:58:10 -0800

    add Current Limits; split ExtensionMotor to class

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -1,19 +1,32 @@
 package frc.robot.subsystems.intake;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+
 import edu.wpi.first.math.util.Units;
 
 public class IntakeConstants {
-  public static enum CAN {
-    ExtensionMotor(11),
-    RollerMotor(12);
+    public static enum CAN {
+        ExtensionMotor(11),
+        RollerMotor(12);
 
-    public int id;
-
-    CAN(int id) {
-      this.id = id;
+        public int id;
+        CAN(int id) {
+            this.id = id;
+        }
     }
-  }
+    public static class ExtensionMotor {
+        public static final double minLength = Units.inchesToMeters(0);
+        public static final double maxLength = Units.inchesToMeters(6);
 
-  static double minLength = Units.inchesToMeters(0);
-  static double maxLength = Units.inchesToMeters(6);
+        public static final double extCurrentLimit = 30;
+
+        public static final CurrentLimitsConfigs extCurrentConfigs =
+            new CurrentLimitsConfigs()
+                .withStatorCurrentLimit(extCurrentLimit)
+                .withSupplyCurrentLimit(extCurrentLimit)
+                .withStatorCurrentLimitEnable(true)
+                .withSupplyCurrentLimitEnable(true)
+                .withSupplyCurrentLowerLimit(30)
+                .withSupplyCurrentLowerTime(0);
+    }
 }
