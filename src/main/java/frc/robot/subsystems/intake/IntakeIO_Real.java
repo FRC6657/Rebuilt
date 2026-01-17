e5d71601df647dec3c9d723db1ce40195ed9388f
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2026-01-17 11:53:15 -0800

    finish IntakeIO_Real.java

--- src/main/java/frc/robot/subsystems/intake/IntakeIO_Real.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO_Real.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.intake;
 
+import org.littletonrobotics.junction.Logger;
+
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
@@ -9,14 +11,19 @@ import com.reduxrobotics.sensors.canandmag.Canandmag;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.RobotController;
 
-public class IntakeIO_Real {
+public class IntakeIO_Real implements IntakeIO {
     private TalonFX extMotor;
 
     private Canandmag encoder;
 
     private double angleSetpoint = IntakeConstants.ExtensionMotor.maxLength;
     private double speedSetpoint = 0;
+
+    private ProfiledPIDController extPID =
+        new ProfiledPIDController(13.5, 0, 0, new Constraints(Units.inchesToMeters(12), Units.inchesToMeters(10))
+    );
 
     public IntakeIO_Real() {
         extMotor = new TalonFX(IntakeConstants.CAN.ExtensionMotor.id);
@@ -34,9 +41,38 @@ public class IntakeIO_Real {
         var extVoltageSignal = extMotor.getMotorVoltage();
         var extCurrentSignal = extMotor.getSupplyCurrent();
 
-        extMotor.optimizeBusUtilization();
+        extVelocitySignal.setUpdateFrequency(IntakeConstants.updateFrequency);
+        extTempSignal.setUpdateFrequency(IntakeConstants.updateFrequency);
+        extVoltageSignal.setUpdateFrequency(IntakeConstants.updateFrequency);
+        extCurrentSignal.setUpdateFrequency(IntakeConstants.updateFrequency);
 
-        changeExtSeptoint(IntakeConstants.ExtensionMotor.minLength);
+        extMotor.optimizeBusUtilization();
+        extPID.enableContinuousInput(0, 2);
+        changeExtSetpoint(IntakeConstants.ExtensionMotor.minLength);
+        extPID.reset(encoder.getAbsPosition());
     }
 
+    @Override
+    public void updateInputs(IntakeIOInputs inputs) {
+        inputs.encoderAbsPosition = Units.metersToInches(encoder.getAbsPosition());
+        inputs.encoderRelPosition = Units.metersToInches(encoder.getPosition());
+        inputs.encoderVelocity = Units.metersToInches(encoder.getVelocity());
+
+        inputs.extMotorVelocity = extMotor.getVelocity().getValueAsDouble();
+        inputs.extMotorTemp = extMotor.getDeviceTemp().getValueAsDouble();
+        inputs.extMotorVoltage = extMotor.get() * RobotController.getBatteryVoltage();
+        inputs.extMotorCurrent = extMotor.getSupplyCurrent().getValueAsDouble();
+        inputs.extMotorSetpoint = speedSetpoint;
+
+        double pidOutput = extPID.calculate(inputs.encoderAbsPosition, angleSetpoint);
+        extMotor.set(pidOutput);
+
+        Logger.recordOutput("Intake/ExtensionPIDOutput", pidOutput);
+        Logger.recordOutput("Intake/ExtensionPIDProfileSetpoint", extPID.getSetpoint().position);
+    }
+
+    @Override
+    public void changeExtSetpoint(double setpoint){
+        speedSetpoint = setpoint;
+    }
 }
