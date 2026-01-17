7c618438471d61c0cba19ff0b08c67b6750b5294
Fabian-Anguiano <fabian.anguiano@uphsconnect.org>
2026-01-17 12:54:03 -0800

    HoodConstants and HoodIO_Real completed except for variable errors

--- src/main/java/frc/robot/subsystems/hood/HoodIO_Real.java
+++ src/main/java/frc/robot/subsystems/hood/HoodIO_Real.java
@@ -1,3 +1,68 @@
 package frc.robot.subsystems.hood;
 
-public class HoodIO_Real {}
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.math.MathUtil;
+
+public class HoodIO_Real implements HoodIO {
+
+    //update placeholder value here
+    TalonFX hoodMotor = new TalonFX(1);
+    private MotionMagicVoltage MotionMagicVoltage =
+        new MotionMagicVoltage(HoodConstants.INITIAL_SETPOINT / 360);
+    
+    public HoodIO_Real() {
+
+        var motorConfigurator = hoodMotor.getConfigurator();
+        var motorConfigs = new TalonFXConfiguration();
+        motorConfigs.Feedback.SensorToMechanismRatio = HoodConstants.GEAR_RATIO;
+        motorConfigs.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        motorConfigs.Slot0 = HoodConstants.motorSlot0;
+        motorConfigs.CurrentLimits = HoodConstants.currentConfigs;
+        motorConfigs.MotionMagic = HoodConstants.MotionMagicConfig;
+        motorConfigurator.apply(motorConfigs);
+
+        var Position = hoodMotor.getPosition();
+        var Temp = hoodMotor.getDeviceTemp();
+        var Voltage = hoodMotor.getMotorVoltage();
+        var Current = hoodMotor.getSupplyCurrent();
+
+        //update placeholder values
+        Temp.setUpdateFrequency(50);
+        Voltage.setUpdateFrequency(50);
+        Current.setUpdateFrequency(50);
+        Position.setUpdateFrequency(50);
+
+        hoodMotor.optimizeBusUtilization();
+
+        hoodMotor.setPosition(HoodConstants.INITIAL_SETPOINT / 360);
+
+        changeSetpoint(HoodConstants.INITIAL_SETPOINT);
+    }
+
+    @Override
+    public void updateInputs(HoodIOInputs inputs) {
+        
+        inputs.Temp = hoodMotor.getDeviceTemp().getValueAsDouble();
+        inputs.Current = hoodMotor.getSupplyCurrent().getValueAsDouble();
+        inputs.Voltage = hoodMotor.getMotorVoltage().getValueAsDouble();
+        inputs.Position = hoodMotor.getPosition().getValueAsDouble() * 360;
+        inputs.Velocity = hoodMotor.getVelocity().getValueAsDouble() * 360;
+        inputs.Accerleration = hoodMotor.getAcceleration().getValueAsDouble() * 360;
+        inputs.Setpoint = MotionMagicVoltage.Position * 360;
+
+        hoodMotor.setControl(MotionMagicVoltage);
+    }
+
+    @Override
+    public void changeSetpoint(double setpoint) {
+        MotionMagicVoltage.Position =
+            MathUtil.clamp(
+                setpoint, 
+                HoodConstants.INITIAL_SETPOINT, 
+                HoodConstants.MAX_STEPOINT) 
+            / 360;
+    }
+}
