6dabe8a4431a28ccca23cb68bb70b52dd73119ed
Andrew-6657 <andrew@frankovic.com>
2026-01-19 15:35:07 -0800

    Added subsystems to Superstructure

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -1,11 +1,17 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.drivebase.Drivebase;
+import frc.robot.subsystems.hood.Hood;
+import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.subsystems.turret.Turret;
 import frc.robot.subsystems.turret.TurretConstants;
 import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
@@ -14,13 +20,20 @@ public class Superstructure {
 
   // Subsystems
   Drivebase drivebase;
+  Turret turret;
+  Hood hood;
+  Shooter shoot;
 
   // Fake test visualization angles (radians)
   private double turretAngle = 0.0;
   private double hoodAngle = 0.0;
+  Pose2d turretPos = drivebase.getPose();
 
-  public Superstructure(Drivebase drivebase) {
+  public Superstructure(Drivebase drivebase, Turret turret, Hood hood, Shooter shoot) {
     this.drivebase = drivebase;
+    this.turret = turret;
+    this.hood = hood;
+    this.shoot = shoot;
   }
 
   @AutoLogOutput(key = "3DComponents")
@@ -37,6 +50,18 @@ public class Superstructure {
       new Pose3d(TurretConstants.TURRET_CENTER, turretRotation),
       new Pose3d(hoodPosition, hoodRotation)
     };
+  }
+
+  public void changeTurretAngle(double radians) {
+    turretPos =
+        drivebase
+            .getPose()
+            .transformBy(
+                new Transform2d(
+                    TurretConstants.TURRET_CENTER.getX(),
+                    TurretConstants.TURRET_CENTER.getY(),
+                    new Rotation2d()));
+    Logger.recordOutput("turretPos", turretPos);
   }
 
   public Command logMessage(String message) {
