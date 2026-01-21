23af71aebc51ad3cf8945d0acd9cbd39e30fe432
madisonh0609 <madison.hutchinson@uphsconnect.org>
2026-01-21 15:55:53 -0800

    get rid of position for tunnel

--- src/main/java/frc/robot/subsystems/tunnel/Tunnel.java
+++ src/main/java/frc/robot/subsystems/tunnel/Tunnel.java
@@ -8,6 +8,7 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import java.lang.System.Logger;
+
 import org.littletonrobotics.junction.AutoLogOutput;
 
 public class Tunnel extends SubsystemBase {
@@ -24,11 +25,6 @@ public class Tunnel extends SubsystemBase {
         () -> {
           io.setSpeed(speed);
         });
-  }
-
-  @AutoLogOutput(key = "Tunnel/AtSetpoint")
-  public boolean atSetpoint() {
-    return MathUtil.isNear(inputs.setpoint, inputs.getPosition());
   }
 
   @Override
