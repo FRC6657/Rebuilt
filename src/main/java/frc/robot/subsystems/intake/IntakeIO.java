33d8e3aa7cec57ca11f6edd0472148f624ffd8ac
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2026-01-21 11:21:49 -0800

    add IO inputs

--- src/main/java/frc/robot/subsystems/intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -12,6 +12,13 @@ public interface IntakeIO {
     public double extMotorCurrent = 0.0; // Amps
     public double extMotorSetpoint = 0.0; // 0 to 6
 
+    public double wheelsMotorPosition = 0.0; // Inches
+    public double wheelsMotorVelocity = 0.0; // Inches per second
+    public double wheelsMotorTemp = 0.0; // Celcius
+    public double wheelsMotorVoltage = 0.0; // Volts
+    public double wheelsMotorCurrent = 0.0; // Amps
+    public double wheelsMotorSetpoint = 0.0; // 0 to 6
+
     public double encoderAbsPosition = 0.0;
     public double encoderRelPosition = 0.0;
     public double encoderVelocity = 0.0;
