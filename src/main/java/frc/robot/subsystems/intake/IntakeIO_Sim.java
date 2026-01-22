8319563be5796bd473926ceb7f6b11385ff1df14
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2026-01-21 16:08:45 -0800

    sim code (WILL NEED CHANGES)

--- src/main/java/frc/robot/subsystems/intake/IntakeIO_Sim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO_Sim.java
@@ -5,7 +5,9 @@ import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import frc.robot.GlobalConstants;
 
 public class IntakeIO_Sim implements IntakeIO {
 
@@ -22,11 +24,16 @@ public class IntakeIO_Sim implements IntakeIO {
 
     @Override
     public void updateInputs(IntakeIOInputs inputs) {
+        extSim.update(1 / GlobalConstants.mainLoopFrequency);
 
+        inputs.extMotorVelocity = extSim.getAngularVelocityRPM();
+        inputs.extMotorVoltage = extSim.getInputVoltage();
+        inputs.extMotorCurrent = extSim.getCurrentDrawAmps();
+        // inputs.extMotorSetpoint = speedSetpoint;
     }
 
     @Override
     public void changeExtSetpoint(double setpoint) {
-
+        //TODO figure out if setpoints are needed
     }
 }
