5f9e90946e21285c3d7390a747108b047b6b3d68
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2026-01-21 17:09:43 -0800

    spotless

--- src/main/java/frc/robot/subsystems/tunnel/Tunnel.java
+++ src/main/java/frc/robot/subsystems/tunnel/Tunnel.java
@@ -4,10 +4,9 @@
 
 package frc.robot.subsystems.tunnel;
 
-import org.littletonrobotics.junction.Logger;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import org.littletonrobotics.junction.Logger;
 
 public class Tunnel extends SubsystemBase {
 
