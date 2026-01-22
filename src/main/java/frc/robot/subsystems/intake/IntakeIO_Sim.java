e8e777d58adba629d2e57ae57c4d45583f6409dd
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2026-01-21 16:01:20 -0800

    configure motor for intake sim (hopefully no wheel motor, needs gearing corrections)

--- src/main/java/frc/robot/subsystems/intake/IntakeIO_Sim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO_Sim.java
@@ -1,12 +1,32 @@
 package frc.robot.subsystems.intake;
 
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+
 public class IntakeIO_Sim implements IntakeIO {
+
+    private ProfiledPIDController extPID = 
+        new ProfiledPIDController(13.5, 0, 0, new Constraints(Units.inchesToMeters(12), Units.inchesToMeters(10))
+    );
+
+    private DCMotorSim extSim = new DCMotorSim(
+        LinearSystemId.createDCMotorSystem(DCMotor.getFalcon500(1), 0.0001, 1), //TODO configure correct gearing
+        DCMotor.getFalcon500(1)
+    );
 
     public IntakeIO_Sim() {}
 
     @Override
-    public void updateInputs(IntakeIOInputs inputs) {}
+    public void updateInputs(IntakeIOInputs inputs) {
+
+    }
 
     @Override
-    public void changeExtSetpoint(double setpoint) {}
+    public void changeExtSetpoint(double setpoint) {
+
+    }
 }
