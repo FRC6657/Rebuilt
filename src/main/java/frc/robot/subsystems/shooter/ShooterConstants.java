12090a62df1f2d2afaf991c407146bc02cb00484
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2026-01-28 16:58:40 -0800

    spotless + shooter error fixed

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems.shooter;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 
 public class ShooterConstants {
-  public static double gearRatio;
+  public static double gearRatio = 1; // TODO: set gear ratio
 
   public static double updateFrequency = 0.0;
   public static CurrentLimitsConfigs currentConfigs;
