f73edc9d9487dd587b307e4d531d08ccba17578c
madisonh0609 <madison.hutchinson@uphsconnect.org>
2026-02-24 16:22:10 -0800

    updated turret full rotation range to 420 deg

--- src/main/java/frc/robot/subsystems/shooter/turret/TurretConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/turret/TurretConstants.java
@@ -26,7 +26,7 @@ public class TurretConstants {
 
   public static final double POSITION_TOLERANCE = 2.0; // degrees
 
-  public static final double FULL_ROTATION_RANGE = 460.0; // degrees
+  public static final double FULL_ROTATION_RANGE = 420.0; // degrees
   public static final double ROTATION_RANGE = FULL_ROTATION_RANGE - 50.0; // degrees
 
   /** Total gear reduction from motor to turret output. */
