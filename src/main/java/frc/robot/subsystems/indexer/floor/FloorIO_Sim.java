22d82f33418943cc9bf59c61c5196ce80d173f2c
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2026-03-14 10:41:43 -0700

    Add second floor motor to sim

--- src/main/java/frc/robot/subsystems/indexer/floor/FloorIO_Sim.java
+++ src/main/java/frc/robot/subsystems/indexer/floor/FloorIO_Sim.java
@@ -14,7 +14,8 @@ import frc.robot.GlobalConstants;
 /** Simulated floor indexer implementation using a DCMotorSim physics model. */
 public class FloorIO_Sim implements FloorIO {
 
-  private TalonFX motor = new TalonFX(GlobalConstants.CAN.Floor_One.id);
+  private TalonFX motorOne = new TalonFX(GlobalConstants.CAN.Floor_One.id);
+  private TalonFX motorTwo = new TalonFX(GlobalConstants.CAN.Floor_Two.id);
   private VoltageOut setpoint = new VoltageOut(0);
 
   private DCMotorSim motorModel =
@@ -24,7 +25,7 @@ public class FloorIO_Sim implements FloorIO {
           FloorConstants.MOTOR);
 
   public FloorIO_Sim() {
-    motor
+    motorOne
         .getConfigurator()
         .apply(
             FloorConstants.CONFIG.withCurrentLimits(
@@ -37,20 +38,29 @@ public class FloorIO_Sim implements FloorIO {
   public void updateInputs(FloorIOInputs inputs) {
 
     // Control the motor
-    motor.setControl(setpoint);
+    motorOne.setControl(setpoint);
 
     // Sim Stuff
-    var motorSim = motor.getSimState();
-    motorSim.setSupplyVoltage(12);
-    motorModel.setInputVoltage(motorSim.getMotorVoltage());
+    var motorOneSim = motorOne.getSimState();
+    motorOneSim.setSupplyVoltage(12);
+    motorModel.setInputVoltage(motorOneSim.getMotorVoltage());
     motorModel.update(1 / GlobalConstants.mainLoopFrequency);
-    motorSim.setRawRotorPosition(motorModel.getAngularPosition().times(FloorConstants.GEAR_RATIO));
-    motorSim.setRotorVelocity(motorModel.getAngularVelocity().times(FloorConstants.GEAR_RATIO));
+    motorOneSim.setRawRotorPosition(motorModel.getAngularPosition().times(FloorConstants.GEAR_RATIO));
+    motorOneSim.setRotorVelocity(motorModel.getAngularVelocity().times(FloorConstants.GEAR_RATIO));
+
+    var motorTwoSim = motorTwo.getSimState();
+    motorTwoSim.setSupplyVoltage(12);
+    motorTwoSim.setRawRotorPosition(motorModel.getAngularPosition().times(FloorConstants.GEAR_RATIO));
+    motorTwoSim.setRotorVelocity(motorModel.getAngularVelocity().times(FloorConstants.GEAR_RATIO));
 
     // Log Data
-    inputs.temp = -1; // Sim has no temps
-    inputs.voltage = motor.getMotorVoltage().getValueAsDouble();
-    inputs.statorCurrent = motor.getStatorCurrent().getValueAsDouble();
+    inputs.motorOneTemp = -1; // Sim has no temps
+    inputs.motorOneVoltage = motorOne.getMotorVoltage().getValueAsDouble();
+    inputs.motorOneStatorCurrent = motorOne.getStatorCurrent().getValueAsDouble();
+
+    inputs.motorTwoTemp = -1; // Sim has no temps
+    inputs.motorTwoVoltage = motorOne.getMotorVoltage().getValueAsDouble();
+    inputs.motorTwoStatorCurrent = motorOne.getStatorCurrent().getValueAsDouble();
   }
 
   @Override
